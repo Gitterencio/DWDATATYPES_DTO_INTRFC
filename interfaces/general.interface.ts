951c2fd2b74052f21df0d22bcaf60df127b03033
Alberto Matamoros <tmatamoros@innovabpo.io>
2023-04-02 14:50:54 -0600

    test

---
+++ interfaces/general.interface.ts
@@ -0,0 +1,15 @@
+export interface Product{
+    readonly name: string;
+    readonly description: string;
+    readonly imageURL: string;
+    readonly price: number;
+    readonly createdAt: Date;
+}
+
+export interface Product2{
+    readonly name: string;
+    readonly description: string;
+    readonly imageURL: string;
+    readonly price: number;
+    readonly createdAt: Date;
+}
