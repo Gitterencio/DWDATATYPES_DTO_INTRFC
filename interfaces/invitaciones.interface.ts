61a83051870499e6df2404034f51b0a49c80571b
Alberto Matamoros <tmatamoros@innovabpo.io>
2023-04-30 11:36:25 -0600

    invitaciones tipos

---
+++ interfaces/invitaciones.interface.ts
@@ -0,0 +1,9 @@
+
+//estado puede ser => ['espera','aceptada','rechazada','cancelada']
+export interface Invitacioness{
+    readonly _id :string;
+    readonly user: string;
+    readonly proyecto: string;
+    readonly estado: string;
+    readonly createdAt: Date;
+}
