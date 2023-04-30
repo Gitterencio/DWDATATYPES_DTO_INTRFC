7da2afcfea2b2099be29120d16d6c1f9c73f9fba
Alberto Matamoros <tmatamoros@innovabpo.io>
2023-04-30 09:57:13 -0600

    proyectos tipos

---
+++ interfaces/proyectos.interface.ts
@@ -0,0 +1,8 @@
+export interface Proyectos{
+    readonly _id :string;
+    readonly name: string;
+    readonly html_text: string;
+    readonly css_text: string;
+    readonly js_text: string;
+    readonly user: string;
+}
