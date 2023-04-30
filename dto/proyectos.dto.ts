cb8e8327fb147d63dee779b032db2c919b97c7f7
Alberto Matamoros <tmatamoros@innovabpo.io>
2023-04-30 10:06:53 -0600

    proyectos tipos

---
+++ dto/proyectos.dto.ts
@@ -0,0 +1,16 @@
+export interface CreateProyectoDTO{
+    readonly name: string;
+    readonly user: string;
+}
+
+export interface SearchProyectoDTO{
+    readonly id: string;
+}
+
+export interface UpdateProyectoDTO{
+    readonly id: string;
+    readonly name: string;
+    readonly html_text: string;
+    readonly css_text: string;
+    readonly js_text: string;
+}
